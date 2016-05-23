6bf395bd08fffab58f0563ebe5e74f195de3f4fd
irishfan686 <pjo5058@gmail.com>
2016-05-22 22:34:00 -0400

    test 3

--- imo/settings.py
+++ imo/settings.py
@@ -129,18 +129,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_URL = '/imo_app/view_login/'
-
-if 'SECRET_KEY' in os.environ:
-    CSRF_COOKIE_SECURE = True
-
-    SESSION_COOKIE_SECURE = True
-
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-
-    SECURE_BROWSER_XSS_FILTER = True
-
-    SECURE_SSL_REDIRECT = True
-
-    CSRF_COOKIE_HTTPONLY = True
-
-    X_FRAME_OPTIONS = 'DENY'
