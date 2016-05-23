cd55f1ead8fcc0cced668cb02ce58346a7ac911f
Myron Leung <myronleung@gmail.com>
2016-05-22 23:09:39 -0400

    testing static heroku

--- imo/settings.py
+++ imo/settings.py
@@ -126,9 +126,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-LOGIN_URL = '/imo_app/view_login/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 if 'SECRET_KEY' in os.environ:
     CSRF_COOKIE_SECURE = True
