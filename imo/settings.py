d670b13413654b9025bf795723f6dd5e5cd04e37
Myron Leung <myronleung@gmail.com>
2016-05-23 00:00:17 -0400

    added whitenoise

--- imo/settings.py
+++ imo/settings.py
@@ -127,7 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, '../imo_app/static'))
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 if 'SECRET_KEY' in os.environ:
     CSRF_COOKIE_SECURE = True
