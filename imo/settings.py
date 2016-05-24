668ed8b369a486b7123013265d609a09dc1c750c
irishfan686 <pjo5058@gmail.com>
2016-05-24 13:05:51 -0400

    attempt # 750

--- imo/settings.py
+++ imo/settings.py
@@ -126,13 +126,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'imo_app/static'),
-)
 
 
 # Simplified static file serving.
