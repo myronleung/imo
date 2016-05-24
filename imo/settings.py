35000b7687c39efb4edcffb4ef1713f26ced90ec
irishfan686 <pjo5058@gmail.com>
2016-05-24 13:17:33 -0400

    754

--- imo/settings.py
+++ imo/settings.py
@@ -130,7 +130,10 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+    # add more paths with static files here
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
