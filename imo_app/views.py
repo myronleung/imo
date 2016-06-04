f359d0ff054a7191a65de9ed4c28f135b6c636b2
irishfan686 <pjo5058@gmail.com>
2016-06-04 16:58:48 -0400

    commented out voting

--- imo_app/views.py
+++ imo_app/views.py
@@ -324,40 +324,40 @@ def edit(request, id=None):
 
                 #update votes
                 #if any change in the choice
-                if choice1 != old_choices[0] or image1 != old_choices[0].image or request.POST.get('image1-clear'):
-                    #remove the votes for that choice from total votes
-                    instance.total_votes = instance.total_votes - old_choices[0].votes
-                    #set total votes to 0
-                    votes1 = 0
-                #else leave it the same
-                else:
-                    votes1 = old_choice[0].votes
-
-                #choice2
-                if choice2 != old_choices[1] or image2 != old_choices[1].image or request.POST.get('image2-clear'):
-                    #remove the votes for that choice from total votes
-                    instance.total_votes = instance.total_votes - old_choices[1].votes
-                    #set total votes to 0
-                    votes2 = 0
-                #else leave it the same
-                else:
-                    votes2 = old_choice[1].votes
-
-                #choice3
-                if choice3 != old_choices[2] or image3 != old_choices[2].image or request.POST.get('image3-clear'):
-                    #remove the votes for that choice from total votes
-                    instance.total_votes = instance.total_votes - old_choices[2].votes
-                    #set total votes to 0
-                    votes3 = 0
-                #else leave it the same
-                else:
-                    votes3 = old_choice[2].votes
+#                if choice1 != old_choices[0] or image1 != old_choices[0].image or request.POST.get('image1-clear'):
+#                    #remove the votes for that choice from total votes
+#                    instance.total_votes = instance.total_votes - old_choices[0].votes
+#                    #set total votes to 0
+#                    votes1 = 0
+#                #else leave it the same
+#                else:
+#                    votes1 = old_choices[0].votes
+#
+#                #choice2
+#                if choice2 != old_choices[1] or image2 != old_choices[1].image or request.POST.get('image2-clear'):
+#                    #remove the votes for that choice from total votes
+#                    instance.total_votes = instance.total_votes - old_choices[1].votes
+#                    #set total votes to 0
+#                    votes2 = 0
+#                #else leave it the same
+#                else:
+#                    votes2 = old_choices[1].votes
+#
+#                #choice3
+#                if choice3 != old_choices[2] or image3 != '' and image3 != old_choices[2].image or request.POST.get('image3-clear'):
+#                    #remove the votes for that choice from total votes
+#                    instance.total_votes = instance.total_votes - old_choices[2].votes
+#                    #set total votes to 0
+#                    votes3 = 0
+#                #else leave it the same
+#                else:
+#                    votes3 = old_choices[2].votes
                 #delete the old choices so we can add the new ones
                 Choice.objects.filter(question=instance).delete()
                 #add the new choices
-                c1 = Choice(question = instance, choice_text = choice1, image = image1, votes = votes1)
-                c2 = Choice(question = instance, choice_text = choice2, image = image2, votes = votes2)
-                c3 = Choice(question = instance, choice_text = choice3, image = image3, votes = votes3)
+                c1 = Choice(question = instance, choice_text = choice1, image = image1) # votes = votes1)
+                c2 = Choice(question = instance, choice_text = choice2, image = image2) # votes = votes2)
+                c3 = Choice(question = instance, choice_text = choice3, image = image3) # votes = votes3)
                 c1.save()
                 c2.save()
                 c3.save()
