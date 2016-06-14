f0ddad7150d2a1591bd5cb4e0f2bcec27cf8e242
myronleung <myronleung@gmail.com>
2016-06-14 23:34:18 +0000

    switched to debug off

--- imo/settings/dev.py
+++ imo/settings/dev.py
@@ -19,3 +19,26 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+# if 'SECRET_KEY' in os.environ:
+SECRET_KEY = 'fl_v9(iu_qm_+mq^7c)bm0qcue(j%3=n63zx-6^&#17^)(ba5f'
+# SECRET_KEY = 'SECRET_KEY' in os.environ
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['174.129.139.15']
+
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+SECURE_BROWSER_XSS_FILTER = True
+
+CSRF_COOKIE_HTTPONLY = True
+
+X_FRAME_OPTIONS = 'DENY'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
